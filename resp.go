62f416c6e512511ff35e9898acbcf40441908e3e
P-pisiko <enesfidan75@proton.me>
2026-02-27 14:15:26 -0500

    helpers to read from buffer

--- resp.go
+++ resp.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bufio"
 	"io"
+	"strconv"
 )
 
 const (
@@ -27,4 +28,32 @@ type Resp struct {
 
 func NewResp(rd io.Reader) *Resp {
 	return &Resp{reader: bufio.NewReader(rd)}
+}
+
+func (r Resp) readLine() (line []byte, n int, err error) {
+	for {
+		b, err := r.reader.ReadByte()
+		if err != nil {
+			return nil, 0, err
+		}
+		n += 1
+		line = append(line, b)
+		if len(line) >= 2 && line[len(line)-2] == '\r' {
+			break
+		}
+
+	}
+	return line[:len(line)-2], n, nil
+}
+
+func (r *Resp) readInteger() (x int, n int, err error) {
+	line, n, err := r.readLine()
+	if err != nil {
+		return 0, 0, err
+	}
+	i64, err := strconv.ParseInt(string(line), 10, 64)
+	if err != nil {
+		return 0, n, err
+	}
+	return int(i64), n, nil
 }
